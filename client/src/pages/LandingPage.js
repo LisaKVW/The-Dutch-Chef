aed1d96194ce53eaa072ced1aa1965f91485fc61
Lisa Wand <lisavenneker21@gmail.com>
2021-01-07 19:40:38 -0500

    landingpage carousel added

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import M from 'materialize-css/dist/js/materialize.min.js'
 
 const LandingPage = (props) => {
+
+  useEffect(() => {
+    let elems = document.querySelector('.carousel');
+    let instances = M.Carousel.init(elems, {});
+  }, [])
+
   return (
     <div>
-      <p1>
-        Welcome to the landingPage
-      </p1>
+      <h2> Welcome to the Dutch Chef!</h2>
+      <div class="carousel">
+        <a class="carousel-item" href="#one!"><img src="https://i.imgur.com/eXOA6Iy.png" /></a>
+        <a class="carousel-item" href="#two!"><img src="https://i.imgur.com/OD0PR9d.png" /></a>
+        <a class="carousel-item" href="#three!"><img src="https://i.imgur.com/yqKktwG.png3" /></a>
+        <a class="carousel-item" href="#four!"><img src="https://i.imgur.com/yIsfdnv.png" /></a>
+        <a class="carousel-item" href="#five!"><img src="https://i.imgur.com/ozBfUqs.jpg" /></a>
+        <a class="carousel-item" href="#five!"><img src="https://i.imgur.com/ir01kAA.png" /></a>
+
+      </div>
     </div>
   )
 }
