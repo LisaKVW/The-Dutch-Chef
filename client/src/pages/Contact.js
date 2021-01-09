f134281c00d6834129a4ad73faa95bba47bed186
Lisa Wand <lisavenneker21@gmail.com>
2021-01-08 20:08:33 -0500

    border and shadow good on recipes

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -9,7 +9,7 @@ const Contact = (props) => {
           <img src="https://i.imgur.com/a1Z0lWa.jpg" style={{ width: "70%", marginTop: "22px", borderRadius: "6px" }} />
         </section>
         <section className="contact-email">
-          <h4> The Dutch chef would love to hear your feedback!</h4>
+          <h4> The Dutch Chef would love to hear your feedback!</h4>
           <p> Please email me with any questions, suggestions or comments to:
         <span style={{ fontSize: "21px" }}> DutchChefContact@gmail.com </span>
           </p>
--- client/src/pages/SingleRecipe.js
+++ client/src/pages/SingleRecipe.js
@@ -24,7 +24,7 @@ const SingleRecipe = (props) => {
 
 
   return (!myRecipe ? <div></div> :
-    <div className="recipe-wrap" style={{ width: "80%", borderRadius: "14px", border: "1px solid red", marginLeft: "10%" }}>
+    <div className="recipe-wrap" style={{ width: "80%", borderRadius: "14px", border: "2px solid white", boxShadow: "1px 8px 8px 8px #eeeeee", marginLeft: "10%" }}>
       <h3>{myRecipe.title}</h3>
       {myRecipe.RecipeImgs.map((image, index) => {
         return <img key={index} src={image.image} alt="food image" style={{ width: "35%", borderRadius: "5px" }} />
@@ -34,19 +34,19 @@ const SingleRecipe = (props) => {
         <div>
           <span style={{ fontWeight: "bold" }}> Ingredients: </span>
         </div>
-        {myRecipe.ingredients.map((ingredients) => <li>  {ingredients} </li>)}
+        {myRecipe.ingredients.map((ingredients) => <li style={{ marginLeft: "10px" }}>  {ingredients} </li>)}
       </ul>
 
       <ul style={{ display: "inline", textAlign: "left" }}>
-        <div className="title-instructions" style={{ marginTop:"8px"}}>
+        <div className="title-instructions" style={{ marginTop: "8px" }}>
           <span style={{ fontWeight: "bold" }}> Instructions: </span>
         </div>
-        {myRecipe.instructions.map((instruction) => <li>  {instruction} </li>)}
+        {myRecipe.instructions.map((instruction) => <li style={{ marginLeft: "10px" }}>  {instruction} </li>)}
       </ul>
-      <p> <span style={{ fontWeight: "bold" }}> Tips: </span>  {myRecipe.tips}</p>
-
+      <div style={{ display: "inline", textAlign: "left" }}>
+        <p > <span style={{ fontWeight: "bold" }}> Tips: </span>  {myRecipe.tips}</p>
+      </div>
     </div>
-
   )
 }
 
