539aa8da03c44d66b942e2ab7ddda9b5941f9239
Lisa Wand <lisavenneker21@gmail.com>
2021-01-09 18:06:08 -0500

    code fixed for images display

--- client/src/pages/SingleRecipe.js
+++ client/src/pages/SingleRecipe.js
@@ -18,6 +18,7 @@ const SingleRecipe = (props) => {
     }
   }
 
+
   useEffect(() => {
     getRecipe()
   }, [props.match.params.dish_id])
@@ -55,3 +56,35 @@ const SingleRecipe = (props) => {
 }
 
 export default SingleRecipe
+
+
+// return (!myRecipe ? <div></div> :
+//   <div className="recipe-wrap" style={{ width: "80%", borderRadius: "14px", border: "2px solid white", boxShadow: "1px 8px 8px 8px #eeeeee", marginLeft: "10%" }}>
+//     <h3>{myRecipe.title}</h3>
+//     {myRecipe.RecipeImgs.map((image, index) => {
+//       return <img key={index} src={image.image} alt="food image" style={{ width: "35%", borderRadius: "5px" }} />
+//     })}
+
+//     <ul style={{ display: "inline", textAlign: "left" }}>
+//       <div>
+//         <span style={{ fontWeight: "bold" }}> Ingredients: </span>
+//       </div>
+//       {myRecipe.ingredients.map((ingredients) => <li style={{ marginLeft: "10px" }}>  {ingredients} </li>)}
+//     </ul>
+
+//     <ul style={{ display: "inline", textAlign: "left" }}>
+//       <div className="title-instructions" style={{ marginTop: "8px" }}>
+//         <span style={{ fontWeight: "bold" }}> Instructions: </span>
+//       </div>
+//       {myRecipe.instructions.map((instruction) => <li style={{ marginLeft: "10px" }}>  {instruction} </li>)}
+//     </ul>
+//     <div style={{ display: "inline", textAlign: "left" }}>
+//       {myRecipe.tips > 1 ? (
+//         <p > <span style={{ fontWeight: "bold" }}> Tips: </span>  {myRecipe.tips}</p>
+//       )
+//         :
+//         <div></div>}
+//     </div>
+//   </div>
+// )
+// }
