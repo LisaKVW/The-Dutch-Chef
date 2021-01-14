d791359af1346799f57593ed88371b315efe1095
Lisa Wand <lisavenneker21@gmail.com>
2021-01-14 16:06:46 -0500

    recipe cards response works and cleaned code

--- client/src/pages/Category.js
+++ client/src/pages/Category.js
@@ -32,25 +32,22 @@ const Category = (props) => {
   // added ternary because - useEffect runs after the return - so without ternary we would have an empty page
   return (!pageloaded ? <div> loading... </div> :
     <div className="wrapper" style={{ margin: "0 auto" }}>
-      <div className="grid-hold" style={{ display: "grid", gridTemplateColumns: "33% 33% 33%", width: "80%", marginTop: "6vh", margin: "0 auto" }}>
+      <div className="grid-hold" style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(270px, 1fr))", width: "80%", marginTop: "6vh", margin: "0 auto", gridGap: "3%" }}>
+
         {chosenCategory.map(dish => {
           return (
             <div className="row">
-              <div className="col s12 m4" style={{ width: "85%" }}>
-
-                <div className="card" style={{ backgroundColor: "black" }}>
-                  <div className="card-image" >
-                    {dish.RecipeImgs.map((pict) => {
-                      const { image } = pict
-                      // console.log(pict)
-                      return <img src={image} />
-                    })}
-                    <div className="card-stacked" style={{ maxHeight: "100px", diplay: "inline-block" }}>
-                      {/* <span class="card-title" style={{ color: "white" }}> {dish.title} </span> */}
-                    </div>
-                    <div className="card-action" style={{ padding: "3% 2%" }}>
-                      <NavLink to={{ pathname: `/recipe/${dish.id}`, state: {} }}>  {dish.title}  </NavLink>
-                    </div>
+              <div className="card" style={{ backgroundColor: "black" }}>
+                <div className="card-image" >
+                  {dish.RecipeImgs.map((pict) => {
+                    const { image } = pict
+                    // console.log(pict)
+                    return <img src={image} />
+                  })}
+                  <div className="card-stacked" style={{ maxHeight: "100px", diplay: "inline-block" }}>
+                  </div>
+                  <div className="card-action" style={{ padding: "3% 2%" }}>
+                    <NavLink to={{ pathname: `/recipe/${dish.id}`, state: {} }}>  {dish.title}  </NavLink>
                   </div>
                 </div>
               </div>
