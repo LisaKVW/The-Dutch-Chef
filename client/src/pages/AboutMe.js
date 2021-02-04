f7b31e3185b31fa0fdd8fe3492b8bc13e6f02709
Lisa Wand <lisavenneker21@gmail.com>
2021-02-04 18:10:22 -0500

    about and contact page better mobile view

--- client/src/pages/AboutMe.js
+++ client/src/pages/AboutMe.js
@@ -12,7 +12,7 @@ const AboutMe = (props) => {
     <div>
       {/* <div className="grid-wrap" style={{ display: "grid", gridTemplateColumns: "50% 50%", width: "80%", marginTop: "5.3vh", margin: "0 auto", gridColumnGap: "2rem" }}> */}
       <h4 style={{ marginBottom: "0" }}> Who is the Dutch chef?</h4>
-      <div className="grid-hold" style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(400px, 1fr))", width: "92%", marginTop: "6vh", margin: "0 auto", gridGap: "2%" }}>
+      <div className="grid-hold" style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))", width: "92%", marginTop: "6vh", margin: "0 auto", gridGap: "2%" }}>
 
         <section style={{ marginRight: "10%", marginTop: "0%" }}>
           < div class="carousel" >
